09bb8651ecc0ac3be91fd146587b95309f4c766a
Omar Loyo <omarloyo121@icloud.com>
2024-03-24 14:38:23 -0600

    added app font, created header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <Layout>
-      Testing
+      
     </Layout>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,24 @@
 const Layout = ({ children }) => {
   return (
     <div className="bg-light_rose">
-      <div>
-        Nav
+      {/* Header */}
+      <div className="bg-header h-auto">
+        <img
+          src="/src/assets/layout/game-logo.png"
+          alt="game logo"
+          className="h-[132px] mx-auto"
+        />
       </div>
-      <div className="container mx-auto">
-        {children}
-      </div>
-      <div>
-        Footer
+      {/* Body */}
+      <div className="container mx-auto">{children}</div>
+      {/* Footer */}
+      <div className="bg-footer h-[132px] flex justify-center items-center">
+        <span className="text-white font-bold text-shadow mt-6">
+          Copyright InnoByte 2024
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
